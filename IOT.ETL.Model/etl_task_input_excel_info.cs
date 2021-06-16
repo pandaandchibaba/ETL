01bbd0de5895531bb47c64f0f9b40f674d67def7
高高 高 <1422957871@qq.com>
2021-06-16 21:34:02 +0800

    高紫如推送

--- IOT.ETL.Model/etl_task_input_excel_info.cs
+++ IOT.ETL.Model/etl_task_input_excel_info.cs
@@ -8,19 +8,17 @@ namespace IOT.ETL.Model
 {
     class etl_task_input_excel_info
     {
-        /// <summary>
-        /// 任务数据输入excel表
-        /// </summary>
+        // 任务数据输入excel表
         public string id { get; set; }
-        public string node_id { get; set; }//节点ID
-        public string node_name { get; set; }//节点名称
-        public string task_id { get; set; }//任务ID
-        public string task_excel_file { get; set; }//excel文件存储位置
-        public string task_excel_sheet { get; set; }//excel文件读取sheet页
-        public string task_excel_id { get; set; }//excel表头信息
-        public string create_by { get; set; }//创建人
-        public DateTime create_time { get; set; }//创建时间
-        public string update_by { get; set; }//更新人
-        public DateTime update_time { get; set; }//更新时间
+        public string node_id { get; set; }           //节点ID
+        public string node_name { get; set; }         //节点名称
+        public string task_id { get; set; }           //任务ID
+        public string task_excel_file { get; set; }   //excel文件存储位置
+        public string task_excel_sheet { get; set; }  //excel文件读取sheet页
+        public string task_excel_id { get; set; }     //excel表头信息
+        public string create_by { get; set; }         //创建人
+        public DateTime create_time { get; set; }     //创建时间
+        public string update_by { get; set; }         //更新人
+        public DateTime update_time { get; set; }     //更新时间
     }
 }
--- IOT.ETL.Repository/ILOGRepository/ILOGRepository.cs
+++ IOT.ETL.Repository/ILOGRepository/ILOGRepository.cs
@@ -13,15 +13,21 @@ namespace IOT.ETL.Repository.ILOGRepository
     {
         //定义缓存关键字
         string redisKey;
+        string redisKey2;
         //获取全部数据
         List<IOT.ETL.Model.etl_data_engine> list = new List<etl_data_engine>();
+
+        List<IOT.ETL.Model.V_IOLG> list2 = new List<V_IOLG>();
         //实例化redis
         RedisHelper<Model.etl_data_engine> rh = new RedisHelper<etl_data_engine>();
 
+        RedisHelper<Model.V_IOLG> rh2 = new RedisHelper<V_IOLG>();
         public ILOGRepository()
         {
             redisKey = "ILOG_list";
+            redisKey2 = "ILOG_list2";
             list = rh.GetList(redisKey);
+            list2 = rh2.GetList(redisKey2);
         }
 
         //添加
@@ -33,7 +39,7 @@ namespace IOT.ETL.Repository.ILOGRepository
                 int i = DapperHelper.Execute(sql);
                 if (i > 0)
                 {
-                    a = DapperHelper.GetList<Model.etl_data_engine>("select * from etl_data_engine order by sid desc LIMIT 1").FirstOrDefault();
+                    a = DapperHelper.GetList<Model.etl_data_engine>("select * from etl_data_engine order by id desc LIMIT 1").FirstOrDefault();
                     //存入
                     list.Add(a);
                     rh.SetList(list, redisKey);
@@ -55,22 +61,73 @@ namespace IOT.ETL.Repository.ILOGRepository
         //删除
         public int DelILOG(string id)
         {
-            string sql = $"delete from etl_data_engine where id='{id}'";
-            return DapperHelper.Execute(sql);
+            try
+            {
+                string sql = $"delete from etl_data_engine where id='{id}'";
+                int i = DapperHelper.Execute(sql);
+                if (i > 0)
+                {
+                    string[] arr = id.Split(',');
+                    foreach (var item in arr)
+                    {
+                        Model.etl_data_engine me = list.FirstOrDefault(x => x.id.Equals(item));
+                        list.Remove(me);
+                    }
+                    //重新存入
+                    rh.SetList(list, redisKey);
+                }
+                return i;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         //显示
         public List<V_IOLG> ShowILOG()
         {
-            string sql = $"select * from V_IOLG";
-            return DapperHelper.GetList<V_IOLG>(sql);
+            try
+            {
+                //判断缓存是否存在
+                if (list2 == null || list2.Count == 0)
+                {
+                    //不存在
+                    list2 = DapperHelper.GetList<IOT.ETL.Model.V_IOLG>("select * from V_IOLG");
+                    //存入
+                    rh2.SetList(list2, redisKey2);
+                }
+                return list2;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         //修改
         public int UptILOG(etl_data_engine a)
         {
-            string sql = $"update etl_data_engine set engine_name='{a.engine_name}',engine_type_id='{a.engine_type_id}',code_type='{a.code_type}',cl_name='{a.cl_name}' where id='{a.id}'";
-            return DapperHelper.Execute(sql);
+            try
+            {
+                string sql = $"update etl_data_engine set engine_name='{a.engine_name}',engine_type_id='{a.engine_type_id}',code_type='{a.code_type}',cl_name='{a.cl_name}' where id='{a.id}'";
+                int i = DapperHelper.Execute(sql);
+                if (i > 0)
+                {
+                    Model.etl_data_engine me = list.FirstOrDefault(x => x.id.Equals(a.id));
+                    list[list.IndexOf(me)] = a;
+                    //重新存入
+                    rh.SetList(list, redisKey);
+                }
+                return i;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
