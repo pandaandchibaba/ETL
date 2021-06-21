582fd6bdf19f3e959987fcb03418ad2accc3e34a
Sweet Dreams <Sweet Dreams@DESKTOP-K7L38MR>
2021-06-21 10:00:43 +0800

    qwc

--- IOT.ETL.Repository/UsersRepository/UsersRepository.cs
+++ IOT.ETL.Repository/UsersRepository/UsersRepository.cs
@@ -49,7 +49,7 @@ namespace IOT.ETL.Repository.UsersRepository
             {
                 string sqll = $"select *from sys_user where username='{username}' and password='{password}'";
                 list = DapperHelper.GetList<Model.sys_user>(sqll);
-                rl.SetList(list, LoginKey);
+                rl.SetList(lstl, LoginKey);
             }
             return i;
         }
